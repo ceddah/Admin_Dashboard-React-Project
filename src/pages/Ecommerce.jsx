455682ab27800e8254f25927f05a4e0cccafe7a3
ceddadh <cedanik1@gmail.com>
2022-06-14 23:39:09 +0200

    theming

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -10,6 +10,7 @@ import {
 import { useStateContext } from "../contexts/ContextProvider";
 
 const ECommerce = () => {
+  const { currentColor } = useStateContext();
   return (
     <div className="mt-12">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
@@ -24,7 +25,7 @@ const ECommerce = () => {
             <Button
               color="white"
               size="md"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
             />
@@ -92,19 +93,19 @@ const ECommerce = () => {
               </div>
               <div className="mt-5">
                 <SparkLine
-                  currentColor="blue"
+                  currentColor={currentColor}
                   id="line-sparkLine"
                   type="Line"
                   height="80px"
                   width="250px"
                   data={SparklineAreaData}
-                  color="blue"
+                  color={currentColor}
                 />
               </div>
               <div className="mt-10">
                 <Button
                   color="'white"
-                  bgColor="blue"
+                  bgColor={currentColor}
                   text="Download Report"
                   bordeRadius="10px"
                 />
